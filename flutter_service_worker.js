cdd991fdadfe22ab663c5d2f34d4737ea72ef461
Kushal Beniwal <kbeniwal2305@gmail.com>
2021-04-08 12:30:10 +0530

    new build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,11 +12,10 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/NOTICES": "e43a60ff52f16b06b319dd983381a010",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a93bce2f4d5aff8c895ece2f01d72d65",
-"/": "a93bce2f4d5aff8c895ece2f01d72d65",
+"index.html": "5558b8c24ec7e2b070284af8e480222e",
+"/": "5558b8c24ec7e2b070284af8e480222e",
 "main.dart.js": "70e540ec3a8bc4c7883f9864213384f5",
 "manifest.json": "7268d731e85213fad6b35f4c3b52f2e6",
 "version.json": "aaafa928a4f8616604eca12d830fe765"
